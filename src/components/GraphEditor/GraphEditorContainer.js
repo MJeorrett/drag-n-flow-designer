fb49c3e4410f9e6673cc04ac76d65b782a05d664
Matthew Jeorrett <Matthew@wallet.services>
2020-05-18 20:34:58 +0100

    Close field on step select

--- src/components/GraphEditor/GraphEditorContainer.js
+++ src/components/GraphEditor/GraphEditorContainer.js
@@ -9,7 +9,10 @@ const mapDispatchToProps = dispatch => ({
     dispatch(actions.steps.add(step));
     dispatch(actions.fields.setSelectedFieldId(null));
   },
-  toggleSelectedStep: stepId => dispatch(actions.steps.toggleSelected(stepId)),
+  toggleSelectedStep: stepId => {
+    dispatch(actions.steps.toggleSelected(stepId));
+    dispatch(actions.fields.setSelectedFieldId(null));
+  },
 });
 
 export default connect(
